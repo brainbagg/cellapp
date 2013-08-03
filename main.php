c0fb79d79e5cf7e34aded57a0537ebb924aaae54
brainbagg <brain4us@hotmail.com>
2013-08-03 11:26:08 -0700

    main.php

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.css" />
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="js/crose-domain.js"></script>
+<script type="text/javascript" src="js/jquery.form.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+</head>
+
+<body>
+<div data-role="page">
+
+<div data-role="header">
+<h1>Cellpad</h1>
+</div>
+
+<div data-role="content">
+
+</div>
+
+<div data-role="footer">
+<h1>Copyright</h1>
+</div>
+
+</div>
+</body>
+</html>
